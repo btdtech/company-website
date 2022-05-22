5c96828358b9e8d82a4ee783ab06d78566a9d976
Bac Trach Dam <bac.dam.1991@gmail.com>
2022-05-23 08:34:19 +1000

    Can retrieve secrets.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,21 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', {structuredData: true});
-  response.send(`Hello ${request.query.name}`);
-});
+
+interface MONGODB_SECRETS {
+  PASSWORD: string;
+}
+
+export const helloWorld = functions
+  .runWith({secrets: ['MONGODB_SECRETS']})
+  .https.onRequest((request, response) => {
+    functions.logger.info('Hello logs!', {structuredData: true});
+
+    let mongoDbSecrets = {} as MONGODB_SECRETS;
+
+    if (process.env.MONGODB_SECRETS) {
+      mongoDbSecrets = JSON.parse(process.env.MONGODB_SECRETS);
+    }
+
+    response.send(`Hello ${mongoDbSecrets.PASSWORD}`);
+  });
